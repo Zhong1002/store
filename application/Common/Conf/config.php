613cfa03d7224cded073f0f06a47db5c4ad4f67c
FanJie <824676495@qq.com>
2017-03-13 15:40:38 +0800

    1

--- application/Common/Conf/config.php
+++ application/Common/Conf/config.php
@@ -80,7 +80,7 @@ $configs= array(
 		'URL_MODULE_MAP' => array('bookadmin'=>'admin'),
 		'APP_SUB_DOMAIN_DEPLOY' => 1, // 开启子域名配置
 		'APP_SUB_DOMAIN_RULES' => array(
-			'detail.ddbookstore.com' => 'Book/Children/index',
+			'detail' => 'Book',
 		),
         
         'VAR_PAGE'				=>"p",
