f526496858f778c8f9a78021acaab16ef2cd8527
FanJie <824676495@qq.com>
2017-04-23 17:18:16 +0800

    前后分离测试

---
+++ data/lang/Book/Lang/zh-cn/admin_menu.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+  'BOOK_MANAGEADMIN_INDEX' => '书籍管理',
+  'BOOK_MANAGEADMIN_ORDER' => '订单管理',
+  'BOOK_ORDERADMIN_INDEX' => '查看订单',
+  'BOOK_SHELVESADMIN_INDEX' => '上架书籍',
+  'BOOK_SHELVESADMIN_RECYCLEBIN' => '书籍回收',
+  'BOOK_ORDERADMIN_RECYCLEBIN' => '订单回收',
+);
